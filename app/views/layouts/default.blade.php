f690b2fae5b74c7ebcb65904ed14cd36556326ea
vishnu <vishnu.kvs@gmail.com>
2014-11-19 21:01:12 +1100

    added bootstrap js

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -1,28 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Todo list App</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Todo list App</title>
 
-    <!-- Bootstrap -->
-    {{ HTML::style('css/styles.css') }}
+  <!-- Bootstrap -->
+  {{ HTML::style('css/styles.css') }}
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-  @include('layouts.header')
-  @yield('content')
+      <![endif]-->
+    </head>
+    <body>
+      @include('layouts.header')
+      @yield('content')
 
-  @include('layouts.footer')
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-  </body>
-</html>
+      @include('layouts.footer')
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+      <!-- Include all compiled plugins (below), or include individual files as needed -->
+      {{ HTML::script('js/bootstrap.min.js') }}
+    </body>
+    </html>
